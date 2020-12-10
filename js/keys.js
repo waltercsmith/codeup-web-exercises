c464bfdc56a7de243dc96b1413acd48a5bbd2beb
Walter Smith <walter.c.smith01@gmail.com>
2020-12-10 12:45:56 -0500

    Mapbox Maps API

---
+++ js/keys.js
@@ -0,0 +1,2 @@
+// This file is for saving API keys, and NOT uploading to GitHub
+let mapboxToken = 'pk.eyJ1Ijoid2FsdGVyY3NtaXRoMDEiLCJhIjoiY2tpajB6ZHg2MGYzZDJxbnhxZzJ6ZGYwOCJ9.3w4vPEdUsAWhrItM2i0W-A'
