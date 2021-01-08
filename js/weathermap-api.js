3bc674bcdd6e7e6387c1ada233b27e6bb5a27e38
Walter Smith <walter.c.smith01@gmail.com>
2021-01-08 15:21:10 -0600

    Weathermap:Appended object properties to table

---
+++ js/weathermap-api.js
@@ -0,0 +1,31 @@
+"use strict"
+
+
+$(document).ready(function () {
+    //AJAX call to the OpenWeather API
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: OPEN_WEATHER_APPID, // API key
+        lat:    29.423017,
+        lon:   -98.48527,
+        units: "imperial"
+    }).done(function (data) {
+        console.log(data)
+        $.each(data.list,function (index,item) {
+            console.log(item)
+            if (index % 8 == 0) {
+                $("#forecast").append("<tr><td>" + item.main.temp + "</td>" + "<td>" + item.name + "</td>" + "<td>" + item.population + "</td>" + "<td>" + item.timezone)
+            }
+        })
+        console.log('5 day forecast',data);
+    });
+
+        // let iconUrl = <img src="http://openweathermap.org/img/wn/' data.weather[0].icon + "02x.png"'>
+
+    mapboxgl.accessToken = mapboxToken;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-98.4916, 29.4252],
+        zoom: 9,
+    });
+});
---
+++ stored code.ts
@@ -0,0 +1,3 @@
+// <script src="js/jquery-2.2.4.js"></script>
+
+// <!--<script src="js/weathermap-api.js"></script>-->
