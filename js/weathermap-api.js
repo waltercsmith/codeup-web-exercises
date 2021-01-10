120f48ebb97e45ae44043bcf8b03eb4b41e6e815
Walter Smith <walter.c.smith01@gmail.com>
2021-01-10 09:05:58 -0600

    Weathermap

--- js/weathermap-api.js
+++ js/weathermap-api.js
@@ -2,6 +2,18 @@
 
 
 $(document).ready(function () {
+    // var data = [
+    //     {dt_txt:"2021-01-10 15:00:00", main: {temp_max:43.95, temp_min:42.38, humidity:94}, wind: {deg:49} pressure:1022},
+    //     {dt_txt:"2021-01-11 15:00:00", main: {temp_max:43.95, temp_min:42.38, humidity:94}, wind: {deg:49} pressure:1022},
+    //     {dt_txt:"2021-01-10 15:00:00", main: {temp_max:43.95, temp_min:42.38, humidity:94}, wind: {deg:49} pressure:1022},
+    //     {dt_txt:"2021-01-10 15:00:00", main: {temp_max:43.95, temp_min:42.38, humidity:94}, wind: {deg:49} pressure:1022},
+    //     {dt_txt:"2021-01-10 15:00:00", main: {temp_max:43.95, temp_min:42.38, humidity:94}, wind: {deg:49} pressure:1022},
+    //
+    //
+    //
+    //
+    //     ]
+
     //AJAX call to the OpenWeather API
     $.get("http://api.openweathermap.org/data/2.5/forecast", {
         APPID: OPEN_WEATHER_APPID, // API key
@@ -13,7 +25,14 @@ $(document).ready(function () {
         $.each(data.list,function (index,item) {
             console.log(item)
             if (index % 8 == 0) {
-                $('.card-body').append(item.dt_txt)
+                $('.card-body').find('.item-dt').append(item.dt_txt);
+                $('.card-body').find('.item-temp-max').append(item.main.temp_max);
+                $('.card-body').find('.item-humidity').append(item.main.humidity);
+                $('.card-body').find('.item-wind').append(item.wind.deg)
+
+
+
+
                 // $('#forecast').append("<tr><td>" + item.dt + '</td>' + "<td>" + item.main.temp_max + "/" + item.main.temp_min + "</td>" + "<td>" + item.main.humidity + "</td>" + "<td>" + item.wind.deg + "</td>" + "<td>" + item.main.pressure)
                 // $("#forecast").append("<tr><td>" + item.main.temp + "</td>" + "<td>" + item.name + "</td>" + "<td>" + item.population + "</td>" + "<td>" + item.timezone)
             }
